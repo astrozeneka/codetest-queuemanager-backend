8032b43eac68b60254821c2d71f4e4bc683468f7
Ryan <Ryan@localhost.com>
2024-04-27 10:18:50 +0700

    Add basic in-app communication system using websocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
@@ -13,6 +13,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Websocket connection reference
+websocket_connection = None
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    global websocket_connection
+    await websocket.accept()
+    websocket_connection = websocket
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+@app.get("/test-send")
+async def test_send_socket():
+    global websocket_connection
+    if websocket_connection:
+        await websocket_connection.send_text("Hello from server")
+        return JSONResponse(content={"status": "success"})
+    else:
+        return JSONResponse(content={"status": "error", "message": "No websocket connection"})
